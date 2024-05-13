1c61f1c8570305eb559866d7a1a7cb491ce3a4a6
tandat17z <letandatdowny2003@gmail.com>
2024-05-14 00:56:05 +0700

    fix trainer

---
+++ main.py
@@ -0,0 +1,81 @@
+import argparse
+import torch
+import random
+import numpy as np
+import os
+from tqdm import tqdm
+
+from dataset_v2 import DatasetImg_v2
+from model.crnn import CRNN
+from model.MyCrnn import MyCRNN
+from dataset import DatasetImg
+from utils.StrLabelConverter import *
+
+from trainer import *
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--train', required=True, help='path to traindata folder')
+parser.add_argument('--test', required=True, help='path to traindata folder')
+parser.add_argument('--alphabet', type=str, default='data/mychar.txt', help='path to char in labels')
+
+parser.add_argument('--imgW', type=int, default=512, help='img width')
+parser.add_argument('--dstype', type=str, default='v1', help='version of dataset')
+parser.add_argument('--model', type=str, default='MyCRNN', help='type of model')
+
+parser.add_argument('--batch_size', type=int, default=64, help='input batch size')
+parser.add_argument('--nepochs', type=int, default=100, help='number of epochs to train for')
+parser.add_argument('--manualSeed', type=int, default=1708, help='reproduce experiemnt')
+parser.add_argument('--pretrained', default='', help="path to pretrained model (to continue training)")
+parser.add_argument('--savedir', default='checkpoint', help="path to savedir ")
+
+parser.add_argument('--num_hidden', type=int, default=200, help='size of the lstm hidden state')
+parser.add_argument('--dropout', type=int, default=0.1, help='dropout')
+parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')
+
+parser.add_argument('--valInterval', type=int, default = 5, help='Interval to be displayed')
+parser.add_argument('--saveInterval', type=int, default = 5, help='Interval to be displayed')
+opt = parser.parse_args()
+
+random.seed(opt.manualSeed)
+np.random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed) # Comment lại để cho khởi tạo tham số ngẫu nhiên
+
+if __name__ == '__main__':
+    device = ( "cuda" if torch.cuda.is_available() else "cpu")
+    print("---------------------------------------------------")
+    print(f"Using {device} device")
+    print("---------------------------------------------------")
+
+    # --------------Tạo Dataset -------------------------------------------------------
+    if opt.dstype == 'v1':
+        print('Sử dụng dataset_v1')
+        train_dataset = DatasetImg(opt.train + '/img', opt.train + '/label', imgW=opt.imgW)
+        test_dataset = DatasetImg(opt.test + '/img', opt.test + '/label', imgW=opt.imgW)
+    elif opt.dstype == 'v2':
+        print('Sử dụng dataset_v2')
+        train_dataset = DatasetImg_v2(opt.train + '/img', opt.train + '/label')
+        test_dataset = DatasetImg_v2(opt.test + '/img', opt.test + '/label')
+
+    with open(os.path.join(opt.alphabet), 'r', encoding='utf-8') as f:
+        alphabet = f.read().rstrip()
+
+    converter = StrLabelConverter(alphabet)
+    print('Num class: ', converter.numClass)
+
+    # --------------------- Create Model ---------------------------------
+    if opt.model == 'MyCRNN':
+        model = MyCRNN(converter.numClass, opt.num_hidden, opt.dropout).to(device)
+    elif opt.model == 'CRNN':
+        model = CRNN(converter.numClass, opt.num_hidden).to(device)
+
+    criterion = torch.nn.CTCLoss().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
+
+    trainer = Trainer(model, optimizer, criterion, converter, opt,
+                      train_dataset,
+                      test_dataset)
+    
+    if opt.pretrained : trainer.load_pretrained(opt.pretrained)
+    
+    trainer.train(opt.nepochs)
--- trainer.py
+++ trainer.py
@@ -3,31 +3,33 @@ from tqdm import tqdm
 from torch.utils.data import random_split
 
 from utils.utils import *
-from tester import Tester
 
 class Trainer:
-    def __init__(self, model, optimizer, criterion, converter, opt):
+    def __init__(self, model, optimizer, criterion, converter, opt,
+                 train_dataset,
+                 test_dataset):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
 
         self.converter = converter
         self.device = next(self.model.parameters()).device
-        self.batch_size = batch_size
+        self.opt = opt
 
         self.train_dataloader = torch.utils.data.DataLoader(
                     train_dataset,
-                    batch_size=self.batch_size,
+                    batch_size=self.opt.batch_size,
                     shuffle=True)
         self.test_dataloader = torch.utils.data.DataLoader(
                     test_dataset,
-                    batch_size=self.batch_size,
+                    batch_size=self.opt.batch_size,
                     shuffle=True)
         self.start_epoch = 0
+        self.log = []
 
     def load_pretrained(self, path):
-        self.checkpoint_path = path
         checkpoint = torch.load(path, map_location=torch.device(self.device))
+
         self.model.load_state_dict(checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         self.start_epoch = checkpoint['epoch']
@@ -37,7 +39,7 @@ class Trainer:
         for epoch in range(self.start_epoch + 1, self.start_epoch + nepochs + 1):
             # Train -----------------------
             print(f'Epoch: {epoch}/{self.start_epoch + nepochs}')
-            avg_loss, avg_levenshtein_loss = self.train()
+            avg_loss, avg_levenshtein_loss = self.epoch_train()
             print('-->>> avg_loss/batch = {:.4f} \t avg_levenshtein_loss/sentence = {:.2f}'.format(avg_loss, avg_levenshtein_loss))
             self.log.append({
                 'type': 'train',
@@ -47,11 +49,35 @@ class Trainer:
             })
 
             # val ------------------------------
-            if epoch % opt.valInterval == 0: 
+            if epoch % self.opt.valInterval == 0: 
                 print("Tester.eval...")
+                avg_loss, avg_levenshtein_loss = self.epoch_eval()
+                print('----->>> avg_loss/batch = {:.4f} \t avg_levenshtein_loss/sentence = {:.2f}'.format(avg_loss, avg_levenshtein_loss))
+                self.log.append({
+                    'type': 'val',
+                    'epoch': epoch,
+                    'avg_Loss': avg_loss,
+                    'levenshtein_Loss': avg_levenshtein_loss
+                })
+
+            # Save --------------------------
+            if epoch % self.opt.saveInterval == 0:
+                print('Saving Model...\n')
+
+                checkpoint = {
+                    'epoch': epoch,
+                    'model_state_dict': self.model.state_dict(),  # Lưu trạng thái của mô hình
+                    'optimizer_state_dict': self.optimizer.state_dict(),  # Lưu trạng thái của optimizer
+                    'log': self.log
+                }
+                directory = self.opt.savedir +'/' + self.opt.model
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                    print(f"Đã tạo thư mục '{directory}'.")
+                torch.save(checkpoint, directory +  f'/checkpoint-{epoch}.pth.tar')
 
 
-    def train(self):
+    def epoch_train(self):
         self.model.train(True)
         avg_loss = 0
         avg_levenshtein_loss = 0
@@ -80,5 +106,42 @@ class Trainer:
 
         avg_loss = avg_loss/self.train_dataloader.sampler.num_samples*self.batch_size
         avg_levenshtein_loss = avg_levenshtein_loss/self.train_dataloader.sampler.num_samples
+
+        return avg_loss, avg_levenshtein_loss
+    
+    def epoch_eval(self, print_ = True):
+        self.model.eval()
+        with torch.no_grad():
+            avg_loss = 0
+            avg_levenshtein_loss = 0
+
+            t = tqdm(iter(self.test_dataloader), total=len(self.test_dataloader))
+            for batch_idx, (imgs, labels) in enumerate(t):
+                imgs = imgs.to(self.device)
+                preds = self.model(imgs)
+                
+                # Compute Loss -------------------------------------------
+                preds_, preds_lengths, targets, target_lengths = GetInputCTCLoss(self.converter, preds, labels)
+                loss = self.criterion(preds_.log_softmax(2), targets, preds_lengths, target_lengths) # ctc_loss chỉ dùng với cpu, dùng với gpu phức tạp hơn thì phải
+                avg_loss += loss.detach().item()
+
+                _, enc_preds = preds.max(2)
+                sim_preds = self.converter.decode(enc_preds.view(-1), preds_lengths, raw = False)
+                avg_levenshtein_loss += self.converter.Levenshtein_loss(sim_preds, labels)
+        
+        avg_loss = avg_loss/self.dataloader.sampler.num_samples * self.opt.batch_size
+        avg_levenshtein_loss = avg_levenshtein_loss/self.dataloader.sampler.num_samples
+
+        # Display ----------------------------------------  
+        if print_:
+            raw_preds = self.converter.decode(enc_preds.view(-1), preds_lengths, raw = True)
+            i = 5
+            for raw_pred, pred, gt in zip(raw_preds, sim_preds, labels):
+                print('='*30)
+                print(f'raw: {raw_pred}')
+                print(f'pred_text: {pred}', )
+                print(f'gt: {gt}')
+                i -= 1
+                if( i == 0): break
 
         return avg_loss, avg_levenshtein_loss
